b32b5eacc7a476752e5965db20f1c29266b311d1
Gaurav Raj <108207597+Gauravraj20@users.noreply.github.com>
2023-02-03 21:27:17 +0530

    upload

--- src/Context/BsState.js
+++ src/Context/BsState.js
@@ -34,6 +34,7 @@ const BsState = (props) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        
       },
       body: JSON.stringify({ movie: movie, slot: time, seats: noOfSeat }),
     });
